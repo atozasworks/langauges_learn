d09252d803d887141d911b78d3d26a42a5d47612
Chaithraa2006 <chaithradhanduguri@gmail.com>
2026-02-28 11:29:04 +0530

    Add initial files for GTongue Learn standalone language learning app
    - Create manifest.json for PWA configuration
    - Add package.json for project dependencies and scripts
    - Implement dialogue.css for styling the dialogue page
    - Create learn-home.css for styling the home page
    - Add general styles in style.css
    - Implement service worker (sw.js) for caching and offline support
    - Create variant.html for navigation testing

---
+++ lan_learn/v2/js/default-learners.js
@@ -0,0 +1,34 @@
+// Default Learners List
+// This file contains the default list of learners that will be loaded when the application starts
+
+const DEFAULT_LEARNERS = [
+    'Akhila',
+    'Navya',
+    'Prashanth',
+    'Tiju',
+    'Reena',
+    'Bhavishya',
+    'Iflaha',
+    'Samsheena',
+    'Sharan',
+    'Geetha',
+    'Shabna',
+    'Vathsala',
+    'Chaithra',
+    'Sowjanya',
+    'Chaitha p',
+    'Safeeda',
+    'Sudeep',
+    'Shaima',
+    'Haseena',
+    'James',
+    'Mubeena',
+    'Thafseeda',
+    'Thomas'
+];
+
+// Export for use in other modules
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = DEFAULT_LEARNERS;
+}
+
